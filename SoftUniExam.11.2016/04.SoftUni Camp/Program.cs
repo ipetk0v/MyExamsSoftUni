794965f4cfbe54bda38bc100c0cd558eff11a926
kimipetk0v <i.petk0v@abv.bg>
2017-01-17 00:12:31 +0200

    new

---
+++ SoftUniExam.11.2016/04.SoftUni Camp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SoftUni_Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            double avtomobil = 0;
+            double mikrobus = 0;
+            double malakavtobus = 0;
+            double golqmavtobus = 0;
+            double vlak = 0;
+
+            for (var i = 0; i < num; i++)
+
+            {
+                double n = double.Parse(Console.ReadLine());
+
+                if (n <= 5)
+                {
+                    avtomobil = avtomobil + n;
+                }
+
+                else if (n >= 6 && n <= 12)
+                {
+                    mikrobus = mikrobus + n;
+                }
+
+                else if (n >= 13 && n <= 25)
+                {
+                    malakavtobus = malakavtobus + n;
+                }
+
+                else if (n >= 26 && n <= 40)
+                {
+                    golqmavtobus = golqmavtobus + n;
+                }
+
+                else
+                {
+                    vlak = vlak + n;
+                }
+            }
+
+            double sum = avtomobil + mikrobus + malakavtobus + golqmavtobus + vlak;
+
+            Console.WriteLine("{0:F2}%", avtomobil / sum * 100);
+            Console.WriteLine("{0:F2}%", mikrobus / sum * 100);
+            Console.WriteLine("{0:F2}%", malakavtobus / sum * 100);
+            Console.WriteLine("{0:F2}%", golqmavtobus / sum * 100);
+            Console.WriteLine("{0:F2}%", vlak / sum * 100);
+        }
+    }
+}
---
+++ SoftUniExam.11.2016/05. Fox/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Fox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int zvezdichki = 1;
+            int tireta = 2 * num - 1;
+            int zvezdichkisreda = tireta / 4;
+            int tiretavkraq = 1;
+            int zvezdivsredata = 2 * num - 1;
+
+            for (var i = 0; i < num; i++)
+            {
+                Console.WriteLine("{0}\\{1}/{0}", new string('*', zvezdichki), new string('-', tireta));
+
+                tireta -= 2;
+                zvezdichki++;
+            }
+
+            for (var y = 0; y < num / 3; y++)
+            {
+                Console.WriteLine("|{0}\\{1}/{0}|", new string('*', zvezdichkisreda), new string('*', zvezdichki - 1));
+
+                zvezdichkisreda++;
+                zvezdichki -= 2;
+            }
+
+
+            for (var t = 0; t < num; t++)
+            {
+                Console.WriteLine("{0}\\{1}/{0}", new string('-', tiretavkraq), new string('*', zvezdivsredata));
+
+                zvezdivsredata -= 2;
+                tiretavkraq++;
+
+
+            }
+        }
+    }
+}
