1b8c5cf8ffa292b29059c069f1c428e473ef6446
kimipetk0v <i.petk0v@abv.bg>
2017-01-19 03:01:36 +0200

    Обновления, добавяне на условия.

---
+++ SoftUniExam.11.2016/06. Battles/06.Battle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Battles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int stop = int.Parse(Console.ReadLine());
+
+            int r = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int y = 1; y <= num2; y++)
+                {
+
+                    r++;
+
+                    if (r > stop)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("({0} <-> {1}) ", i, y);
+                    }
+                }
+            }
+
+        }
+    }
+}
