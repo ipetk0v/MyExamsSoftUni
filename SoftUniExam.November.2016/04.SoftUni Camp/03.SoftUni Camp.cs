50bef4c639a6f2415e3fc0f66b2c85eba8a147a0
kimipetk0v <i.petk0v@abv.bg>
2017-01-19 03:55:01 +0200

    modification

---
+++ SoftUniExam.November.2016/04.SoftUni Camp/03.SoftUni Camp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SoftUni_Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            double cars = 0;
+            double van = 0;
+            double smallVan = 0;
+            double bigVan = 0;
+            double train = 0;
+
+            for (var i = 0; i < num; i++)
+
+            {
+                double n = double.Parse(Console.ReadLine());
+
+                if (n <= 5)
+                {
+                    cars = cars + n;
+                }
+
+                else if (n >= 6 && n <= 12)
+                {
+                    van = van + n;
+                }
+
+                else if (n >= 13 && n <= 25)
+                {
+                    smallVan = smallVan + n;
+                }
+
+                else if (n >= 26 && n <= 40)
+                {
+                    bigVan = bigVan + n;
+                }
+
+                else
+                {
+                    train = train + n;
+                }
+            }
+
+            double sum = cars + van + smallVan + bigVan + train;
+
+            Console.WriteLine("{0:F2}%", cars / sum * 100);
+            Console.WriteLine("{0:F2}%", van / sum * 100);
+            Console.WriteLine("{0:F2}%", smallVan / sum * 100);
+            Console.WriteLine("{0:F2}%", bigVan / sum * 100);
+            Console.WriteLine("{0:F2}%", train / sum * 100);
+        }
+    }
+}
