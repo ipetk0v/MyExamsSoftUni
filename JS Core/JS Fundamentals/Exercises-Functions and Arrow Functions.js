a2ebd9d696f249a7ed92f5cf36c7f68859ee940e
Ivan Petkov <i.petk0v@abv.bg>
2018-01-24 17:03:15 +0200

    Add new solve problems

--- JS Core/JS Fundamentals/Exercises-Functions and Arrow Functions.js
+++ JS Core/JS Fundamentals/Exercises-Functions and Arrow Functions.js
@@ -152,4 +152,230 @@ function modifyAverage(number) {
     }
 }
 
-modifyAverage(5835)
+// 6.	Validity Checker
+// Write a JS program that receives two points in the format [x1, y1, x2, y2] and checks if the distances between each point and the start of the cartesian coordinate system (0, 0) and between the points themselves is valid. A distance between two points is considered valid, if it is an integer value. In case a distance is valid write "{x1, y1} to {x2, y2} is valid", in case the distance is invalid write "{x1, y1} to {x2, y2} is invalid". 
+// The order of comparisons should always be first {x1, y1} to {0, 0}, then {x2, y2} to {0, 0} and finally {x1, y1} to {x2, y2}. 
+// The input consists of two points given as an array of numbers.
+// For each comparison print on the output either "{x1, y1} to {x2, y2} is valid" if the distance between them is valid, or "{x1, y1} to {x2, y2} is invalid"- if it’s invalid.
+
+function validityChecker(input) {
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+ 
+    function distance(x1, y1, x2, y2) {
+        let distH = x1 - x2;
+        let distY = y1 - y2;
+        return Math.sqrt(distH**2 + distY**2);
+    }
+
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+ 
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+ 
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+// 7.	Treasure Locator
+// You will be given a series of coordinates, leading to a buried treasure. Use the map to the right to write a program that locates on which island it is. After you find where all the treasure chests are, compose a list and print it on the console. If a chest is not on any of the islands, print “On the bottom of the ocean” to inform your treasure-hunting team to bring diving gear. If the location is on the shore (border) of the island, it’s still considered to lie inside.
+// The input comes as an array with a variable number of elements that are numbers. Each pair is the coordinates to a buried treasure chest.
+// The output is a list of the locations of every treasure chest, either the name of an island or “On the bottom of the ocean”, printed on the console.
+
+function treasureLocator(input) {
+    for (let index = 0; index < input.length; index += 2) {
+        let y = input[index]
+        let x = input[index + 1]
+
+        checkLocation(x,y)
+    }
+
+    function checkLocation(x,y) {
+        if ((y >= 8 && y <= 9) && (x <= 0 && x >= 1)) {
+            console.log("Tokelou")
+        } else if ((y >= 1 && y <= 3) && (x >= 1 && x <= 3)){
+            console.log("Tuvalu")
+        } else if ((y >= 5 && y <= 7) && (x >= 3 && x <= 6)){
+            console.log("Samoa")
+        } else if ((y >= 0 && y <= 2) && (x >= 6 && x <= 8)){
+            console.log("Tonga")
+        } else if ((y >= 4 && y <= 9) && (x >= 7 && x <= 8)){
+            console.log("Cook")
+        } else {
+            console.log("On the bottom of the ocean")
+        }
+    }
+}
+
+// 8.	Trip Length
+// You will be given the coordinates of 3 points on a 2D plane. Write a program that finds the two shortest segments that connect them (without going back to the starting point). When determining the listing order, use the order with the lowest numerical value (see the figure in the hints for more information).
+// The input comes as an array of 6 numbers. The order is [x1, y1, x2, y2, x3, y3].
+// The output is the return value of your program as a string, representing the order in which the three points must be visited and the final distance between them. See the examples for more info.
+
+
+function tripLength(input) {
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+    let x3 = Number(input[4]);
+    let y3 = Number(input[5]);
+ 
+    let distance12 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    let distance23 = Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2));
+    let distance13 = Math.sqrt(Math.pow((x3 - x1), 2) + Math.pow((y3 - y1), 2));
+ 
+ 
+    if ((distance12 <= distance13) && (distance13 => distance23)) {
+       let a = distance12 + distance23;
+        console.log('1->2->3: ' + a);
+    }
+    else if ((distance12 <= distance23) && (distance13 < distance23)) {
+        let b = distance13 + distance12;
+        console.log('2->1->3: '+ b);
+    }
+    else {
+        let c = distance23 + distance13;
+        console.log('1->3->2: ' + c);
+    }
+ 
+}
+
+// 9.	*Radio Crystals
+// It’s time to put your skills to work for the war effort – creating management software for a radio transmitter factory. Radios require a finely tuned quartz crystal in order to operate at the correct frequency. The resource used to produce them is quartz ore that comes in big chunks and needs to undergo several processes, before it reaches the desired properties.
+// You need to write a JS program that monitors the current thickness of the crystal and recommends the next procedure that will bring it closer to the desired frequency. To reduce waste and the time it takes to make each crystal your program needs to complete the process with the least number of operations. Each operation takes the same amount of time, but since they are done at different parts of the factory, the crystals have to be transported and thoroughly washed every time an operation different from the previous must be performed, so this must also be taken into account. When determining the order, always attempt to start from the operation that removes the largest amount of material.
+// The different operations you can perform are the following:
+// •	Cut – cuts the crystal in 4
+// •	Lap – removes 20% of the crystal’s thickness
+// •	Grind – removes 20 microns of thickness
+// •	Etch – removes 2 microns of thickness
+// •	X-ray – increases the thickness of the crystal by 1 micron; this operation can only be done once!
+// •	Transporting and washing – removes any imperfections smaller than 1 micron (round down the number); do this after every batch of operations that remove material
+// At the beginning of your program, you will receive a number representing the desired final thickness and a series of numbers, representing the thickness of crystal ore in microns. Process each chunk and print to the console the order of operations and number of times they need to be repeated to bring them to the desired thickness.
+// The input comes as a numeric array with a variable number of elements. The first number is the target thickness and all following numbers are the thickness of different chunks of quartz ore.
+// The output is the order of operation and how many times they are repeated, every operation on a new line. See the examples for more information.
+
+
+function radioCrystal(input){
+    var target = Number(input[0]);
+ 
+    function process(crystalThickness, action){
+        switch(action) {
+            case "cut":
+                crystalThickness = crystalThickness >> 2;
+                cutCount++;
+                break;
+            case "lap":
+                crystalThickness *= 0.8;
+                lapCount++;
+                break;
+            case "grind":
+                crystalThickness -= 20;
+                grindCount++;
+                break;
+            case "etch":
+                crystalThickness -= 2;
+                etchCount++;
+                break;
+            case "xRay":
+                crystalThickness += 1;
+                xrayCount++;
+                return crystalThickness;
+        }
+ 
+        return transportingWashing(crystalThickness);
+    }
+ 
+    function transportingWashing(crystalThickness) {
+        return Math.floor(crystalThickness);
+    }
+ 
+    for(let i = 1; i < input.length; i++){
+        var current = input[i];
+        var cutCount = 0, lapCount = 0, grindCount = 0, etchCount = 0, xrayCount = 0;
+        var used = false;
+ 
+        console.log(`Processing chunk ${current} microns`);
+ 
+        while(current != target){
+            while(current >> 2 >= target - 1){
+                current = process(current,"cut");
+            }
+            while(current * 0.8 >= target - 1){
+                current = process(current,"lap");
+            }
+            while(current - 20 >= target - 1){
+                current = process(current,"grind");
+            }
+            while(current - 2 >= target - 1){
+                current = process(current,"etch");
+            }
+            if(current + 1 == target){
+                current = process(current,"xRay");
+                used = true;
+            }
+        }
+ 
+        if(cutCount > 0) {
+            console.log(`Cut x${cutCount}`)
+            console.log("Transporting and washing");
+        }
+ 
+        if(lapCount > 0) {
+            console.log(`Lap x${lapCount}`)
+            console.log("Transporting and washing");
+        }
+ 
+        if(grindCount > 0) {
+            console.log(`Grind x${grindCount}`)
+            console.log("Transporting and washing");
+        }
+ 
+        if(etchCount > 0) {
+            console.log(`Etch x${etchCount}`)
+            console.log("Transporting and washing");
+        }
+ 
+        if(used) {
+            console.log(`X-ray x1`)
+        }
+ 
+        console.log(`Finished crystal ${target} microns`)
+    }
+}
+
+// 10.	**DNA Helix
+// Write a JS program that prints a DNA helix with length, specified by the user. The helix has a repeating structure, but the symbol in the chain follows the sequence ATCGTTAGGG. See the examples for more information.
+// The input comes as a single number. It represents the length of the required helix.
+// The output is the completed structure, printed on the console.
+
+function dnaHelix(number) {
+    number = Number(number);
+    let str = 'ATCGTTAGGG';
+    let counter = 0;
+    for(let i=0; i<number; i++) {
+        if(i%4 == 0) {
+            console.log(`**${str[counter%10]}${str[counter % 10 + 1]}**`);
+        } else if(i%4 == 1) {
+            console.log(`*${str[counter%10]}--${str[counter % 10 + 1]}*`);
+        } else if(i%4 == 2) {
+            console.log(`${str[counter%10]}----${str[counter%10 + 1]}`);
+        } else if(i%4 == 3) {
+            console.log(`*${str[counter%10]}--${str[counter % 10 + 1]}*`);
+        }
+
+        counter += 2
+    }
+}
