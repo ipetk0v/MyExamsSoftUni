ae26c2c2c81a253a56940eda5a0f61a2bdda4f25
Ivan Petkov <i.petk0v@abv.bg>
2018-02-24 16:35:16 +0200

    Add last exercises

--- JS Core/JS Advance/jQuery DOM and Events - Exercises/4.Form Validation/formValidation.js
+++ JS Core/JS Advance/jQuery DOM and Events - Exercises/4.Form Validation/formValidation.js
@@ -1,8 +1,80 @@
 function validate() {
-    $("#userInfo").validate({
-        username: {
-            minlength: 3,
-            maxLength: 20
+    // get all html to jquery elements
+    let username = $('#username'),
+        email = $('#email'),
+        password = $('#password'),
+        confirmPassword = $('#confirm-password'),
+        companyCheckbox = $('#company'),
+ 
+        companyInfo = $('#companyInfo'),
+        companyNumber = $('#companyNumber'),
+ 
+        submitBtn = $('#submit'),
+ 
+        validDiv = $('#valid');
+ 
+    //set global valid
+    let isValid = true;
+ 
+    //checkbox make company div visible
+    companyCheckbox.on("change", function () {
+        if (companyCheckbox.is(':checked')) {
+            companyInfo.css('display', 'block');
+        } else {
+            companyInfo.css('display', 'none');
         }
     });
+ 
+    // validate all dislayed fields on submit
+    submitBtn.on('click', function (ev) {
+        //prevent default form behaviour (refresh page)
+        ev.preventDefault();
+        validateForm();
+ 
+        if (isValid) {
+            validDiv.css('display', 'block');
+        } else {
+            validDiv.css('display', 'none');
+        }
+    });
+ 
+    function validateForm() {
+        isValid = true;
+ 
+        if (companyCheckbox.is(':checked')) {
+            validateCompanyNumber();
+        }
+ 
+        validateInputWithRegex(username, /^[A-Za-z\d]{3,20}$/g);
+        validateInputWithRegex(email, /^.*?@.*?\..*$/g);
+ 
+        if (password.val() === confirmPassword.val()) {
+            validateInputWithRegex(password, /^\w{5,15}$/g);
+            validateInputWithRegex(confirmPassword, /^\w{5,15}$/g);
+        } else {
+            password.css('border', 'solid red');
+            confirmPassword.css('border', 'solid red');
+            isValid = false;
+        }
+    }
+ 
+ 
+    function validateInputWithRegex(input, pattern) {
+        if (pattern.test(input.val())) {
+            input.css('border', 'none');
+        } else {
+            input.css('border', 'solid red');
+            isValid = false;
+        }
+    }
+ 
+    function validateCompanyNumber() {
+        let numValue = +companyNumber.val();
+        if (numValue >= 1000 && numValue <= 9999) {
+            companyNumber.css('border', 'none');
+        } else {
+            companyNumber.css('border', 'solid red');
+            isValid = false;
+        }
+    }
 }
