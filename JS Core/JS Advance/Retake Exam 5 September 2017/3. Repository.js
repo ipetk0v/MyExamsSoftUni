7fdd1486eb083b3e0ccacd89c75ceb196605d72d
Ivan Petkov <i.petk0v@abv.bg>
2018-03-10 23:56:09 +0200

    Add task

--- JS Core/JS Advance/Retake Exam 5 September 2017/3. Repository.js
+++ JS Core/JS Advance/Retake Exam 5 September 2017/3. Repository.js
@@ -1,30 +1,50 @@
 class Repository { 
-    constructor(name,age,birthday){
-        this.name = name
-        this.age = age
-        this.birthday = birthday
-        _data = new Map()        
+    constructor(properties){
+        this.properties = properties
+        this.Data = new Map() 
+        this.mapCount = 0      
     }
-
+    
     add(entity){
-
+        let IsInccorect = false;
+        let entityProp = Object.getOwnPropertyNames(entity)
+        let propertiesProp = Object.getOwnPropertyNames(this.properties)
+        if(entityProp.toString() !== propertiesProp.toString())
+        {
+            propertiesProp.forEach(a => {
+                if(!entityProp.includes(a)){
+                    IsInccorect = true;
+                    throw new Error(`Property ${a} is missing from the entity!`)
+                }
+            })
+        }
+        for (let ent in entity) {
+            if(typeof entity[ent] !== this.properties[ent]){
+                IsInccorect = true;
+                throw new TypeError(`Property ${ent} is of incorrect type!`)
+            }
+        }
+        if(!IsInccorect){            
+         this.Data.set(this.mapCount,entity)
+         this.mapCount++
+        }
+        return this.mapCount
     }
-
     get(id){
-
+        if(!this.Data.has(id) && id < 0){
+            throw new Error(`Entity with id: ${id} does not exist!`)
+        }
+        return this.Data.get(id)
     }
-
     update(id,newEntity){
-
+        this.Data.set(id,newEntity)
     }
-
     del(id){
-
+        this.Data.delete(id)
     }
-
-    get count {
-        
-    }
+    get count(){
+        return this.Data.size
+    }   
 }
 
 
@@ -60,17 +80,17 @@ console.log(repository.get(1));
 // Delete an entity
 repository.del(0);
 console.log(repository.count); // Returns 1
-let anotherEntity = {
-    name1: 'Nakov',
-    age: 26,
-    birthday: new Date(1991, 0, 21)
-};
-repository.add(anotherEntity); // should throw an Error
-anotherEntity = {
-    name: 'Nakov',
-    age: 26,
-    birthday: 1991
-};
-repository.add(anotherEntity); // should throw a TypeError
-repository.del(-1); // should throw Error for invalid id
+// let anotherEntity = {
+//     name1: 'Nakov',
+//     age: 26,
+//     birthday: new Date(1991, 0, 21)
+// };
+// repository.add(anotherEntity); // should throw an Error
+// anotherEntity = {
+//     name: 'Nakov',
+//     age: 26,
+//     birthday: 1991
+// };
+// repository.add(anotherEntity); // should throw a TypeError
+// repository.del(-1); // should throw Error for invalid id
 
